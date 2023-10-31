9be4dbb523bc11fee865916a12479a57189e5cbc
Leandro Alencar <zzleandrobritozz@gmail.com>
2023-10-31 20:21:13 -0300

    feat(db): create crud for client and account

--- projeto-gerente/src/Main.java
+++ projeto-gerente/src/Main.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
+import view.MenuInicial;
 
 /**
  *
@@ -15,6 +11,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Teste");
+        MenuInicial menu = new MenuInicial();
+        menu.setVisible(true);
         // TODO code application logic here
     }
     
